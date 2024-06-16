b29b40a0abd44f4eadbbf98e9a1ea995ecdff1a9
Brett Chapin <brett.chapin89@gmail.com>
2024-06-15 20:23:59 -0400

    Finished Twitter API #15 - Profile Image Upload

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
 const User = require('./../models/user')
 
 const router = new express.Router()
+
+const upload = multer({
+    limits: {
+        fileSize: 100_000_000
+    }
+})
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -61,6 +69,23 @@ router.get('/users/:id', async (req, res) => {
     } catch (error) {
         res.status(500).send(error)
     }
+})
+
+router.post('/users/me/avatar', upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250}).png().toBuffer()
+
+    // if (req.user.avatar != null) {
+    //     req.user.avatar = null
+    //     req.user.avatarExists = false
+    // }
+
+    // req.user.avatar = buffer
+    // req.user.avatarExists = true
+    // await req.user.save()
+
+    res.send(buffer)
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
 module.exports = router
